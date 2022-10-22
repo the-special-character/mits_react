82a3987da6b17161d0a8121c8e1d5938ce27b16d
Yagnesh Modh <yagnesh.modh@gmail.com>
2022-10-22 13:40:53 +0530

    commit code for mounting life cycle method

--- src/App1.jsx
+++ src/App1.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, createRef } from "react";
 
 // properties
 // method
@@ -7,10 +7,73 @@ import React, { Component } from "react";
 // props change
 // state change
 
+// 4 Stages
+
+// 1. Mounting
+
+// before html converts in DOM element
+
+// -> constructor
+// -> getDerivedStateFromProps
+// -> render
+
+// Updating
+
+// UnMounting
+
+// Error
+
+// Props are immutable
+//
+
 class App1 extends Component {
-  state = {
-    counter: 0,
-  };
+  // base on props assign new state value
+  // can call only once
+  h1Ref = createRef();
+  counterRef = createRef();
+
+  constructor(props) {
+    super(props);
+    console.log("constructor", props);
+    this.state = {
+      counter: 0,
+      greet: `Hello, ${props.name}`,
+      comment: null,
+    };
+
+    // this.h1Ref = createRef();
+    // this.counterRef = createRef();
+    // console.log("constructor", document.getElementById("heading"))
+    // this.increment = this.increment.bind(this)
+    // this.decrement = this.decrement.bind(this)
+  }
+
+  // not call once infact it will call on evety state or props change
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log("getDerivedStateFromProps", nextProps);
+    // console.log("getDerivedStateFromProps", document.getElementById("heading"))
+    return {
+      greet: `Hello, ${nextProps.name}`,
+    };
+  }
+
+  // call only once Like constructor
+  async componentDidMount() {
+    console.time("findElement");
+    // console.log("componentDidMount", document.getElementById("heading"))
+    this.h1Ref.current.style.color = "red";
+    this.counterRef.current.style.color = "blue";
+    this.counterRef.current.style.fontSize = "24px";
+    console.timeEnd("findElement");
+    try {
+      const res = await fetch(
+        "https://jsonplaceholder.typicode.com/comments/1"
+      );
+      const json = awahttps://jsonplaceholder.typicode.com/comments/1it res.json();
+      this.setState({ comment: json });
+      console.log(json);
+    } catch (error) {}
+  }
 
   increment = () => {
     this.setState(({ counter }) => {
@@ -24,14 +87,23 @@ class App1 extends Component {
     });
   };
 
+  // render html into DOM
   render() {
     console.log("render");
-    const { name, designation } = this.props;
-    const { counter } = this.state;
+    // console.log("render", document.getElementById("heading"))
+    const { designation } = this.props;
+    const { counter, greet, comment } = this.state;
 
     return (
       <>
-        <h1>{name}</h1>
+        <h1 ref={this.h1Ref}>{greet}</h1>
+        {comment && (
+          <>
+            <h2>{comment.name}</h2>
+            <a href={`mailto:${comment.email}`}>{comment.email}</a>
+            <p>{comment.body}</p>
+          </>
+        )}
         <p
           style={{
             textDecoration: "underline",
@@ -40,7 +112,7 @@ class App1 extends Component {
           {designation}
         </p>
         <button onClick={this.increment}>Increment</button>
-        <p>{counter}</p>
+        <p ref={this.counterRef}>{counter}</p>
         <button onClick={this.decrement}>Decrement</button>
         {counter > 5 ? (
           <h2>Counter is greater then 5</h2>
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import App1 from './App1'
+import App1 from "./App1";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <div>
-    <App name="Virat Kohli" designation="Software Engineer" />
-    <App1 name="Rohit Sharma" designation="Indian Caption" />
-  </div>
-);
+// App1.getDerivedStateFromProps = (nextProps, prevState) => {
+//   console.log("getDerivedStateFromProps", nextProps);
+//   return {
+//     greet: `Hello, ${nextProps.name}`,
+//   };
+// };
+
+class Main extends Component {
+  state = {
+    name: "Rohit Sharma",
+  };
+
+  changeName = () => {
+    this.setState({ name: "Shikhar Dhavan" });
+  };
+
+  render() {
+    const { name } = this.state;
+    return (
+      <div>
+        <App name="Virat Kohli" designation="Software Engineer" />
+        {/* this is instance of App1 component */}
+        {/* const app1 = new App1({ name: name, designation: "Indian Caption"}) */}
+        <App1 name={name} designation="Indian Caption" />
+
+        {/* <App1 name="Rishab Panth" designation="Wicket keeper" /> */}
+
+        <button type="button" onClick={this.changeName}>
+          Change Name
+        </button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Main />);
